6e7108894146304a3517909d98cdbc75067d1650
fuyachen <294143413@qq.com>
2023-03-30 16:38:15 +0800

    欢迎页面样式

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    <router-link to="/login">登录</router-link>
+  <div class="welcome-container">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">通用后台管理系统</div>
+      <div class="description">Vue3.0+ElementPlus+Koa+MongoDB</div>
+    </div>
+    <div class="img"></div>
   </div>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {}
-  },
-}
-</script>
+<script setup></script>
 
-<style scoped></style>
+<style lang="scss">
+.welcome-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  padding: 0 100px;
+  .content {
+    position: relative;
+    bottom: 60px;
+    .sub-title {
+      font-size: 30px;
+      line-height: 45px;
+      color: #5c5a5a;
+    }
+    .title {
+      font-size: 40px;
+      line-height: 70px;
+      color: #90a0d3;
+    }
+    .description {
+      font-size: 15px;
+      line-height: 20px;
+      text-align: left;
+      color: #999;
+    }
+  }
+  .img {
+    background: url(../assets/images/bg.png) no-repeat center/cover;
+    width: 420px;
+    height: 360px;
+  }
+}
+</style>
