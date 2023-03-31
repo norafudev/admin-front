627d48ee228c73d75e1aa865d4cad1ffea0636a2
fuyachen <294143413@qq.com>
2023-03-31 21:48:42 +0800

    注册element图标

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue"
 import router from "./router"
 import ElementPlus from "element-plus"
 import "element-plus/theme-chalk/index.css"
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 import storage from "./utils/storage"
 import api from "./api"
 import store from "./store"
@@ -11,5 +12,9 @@ const app = createApp(App)
 
 // 注册全局方法
 app.config.globalProperties.$storage = storage
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(router).use(ElementPlus).use(store).mount("#app")
