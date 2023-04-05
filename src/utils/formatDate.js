cde832e09ea1a16c045499d1d331e37f1752ca26
fuyachen <294143413@qq.com>
2023-04-05 20:36:24 +0800

    时间格式化

---
+++ src/utils/formatDate.js
@@ -0,0 +1,31 @@
+/**
+ * 工具函数封装
+ */
+
+const formatDate = (date, rule) => {
+  let fmt = rule || "yyyy-MM-dd hh:mm:ss"
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, date.getFullYear())
+  }
+  const o = {
+    // 'y+': date.getFullYear(),
+    "M+": date.getMonth() + 1,
+    "d+": date.getDate(),
+    "h+": date.getHours(),
+    "m+": date.getMinutes(),
+    "s+": date.getSeconds(),
+  }
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      const val = o[k] + ""
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? val : ("00" + val).substr(val.length)
+      )
+    }
+  }
+  return fmt
+  // return fmt.substr(0, 10)
+}
+
+export default formatDate
