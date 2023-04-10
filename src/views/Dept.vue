a9ef1e93fc694b77bb44ce848086e062ffe30b91
fuyachen <294143413@qq.com>
2023-04-10 13:30:41 +0800

    部门创建、编辑、删除

--- src/views/Dept.vue
+++ src/views/Dept.vue
@@ -16,7 +16,7 @@
     </div>
     <div class="base-table">
       <div class="action">
-        <el-button type="primary">创建</el-button>
+        <el-button type="primary" @click="handleOpen">创建</el-button>
       </div>
       <el-table
         :data="deptList"
@@ -31,16 +31,71 @@
         ></el-table-column>
         <el-table-column label="操作">
           <template #default="scope">
-            <el-button size="mini" type="primary" @click="handleEdit(scope.row)"
+            <el-button type="primary" @click="handleEdit(scope.row)"
               >编辑</el-button
             >
-            <el-button size="mini" type="danger" @click="handleDel(scope.row)"
+            <el-button type="danger" @click="handleDel(scope.row._id)"
               >删除</el-button
             >
           </template>
         </el-table-column>
       </el-table>
     </div>
+    <el-dialog
+      :title="action == 'create' ? '创建部门' : '编辑部门'"
+      v-model="showModal"
+    >
+      <el-form
+        ref="dialogForm"
+        :model="deptForm"
+        :rules="rules"
+        label-width="120px"
+      >
+        <el-form-item label="上级部门" prop="parentId">
+          <el-cascader
+            placeholder="请选择上级部门"
+            v-model="deptForm.parentId"
+            :props="{ checkStrictly: true, value: '_id', label: 'deptName' }"
+            clearable
+            :options="deptList"
+            :show-all-levels="true"
+          ></el-cascader>
+        </el-form-item>
+        <el-form-item label="部门名称" prop="deptName">
+          <el-input
+            placeholder="请输入部门名称"
+            v-model="deptForm.deptName"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="负责人" prop="user">
+          <el-select
+            placeholder="请选择部门负责人"
+            v-model="deptForm.user"
+            @change="handleUser"
+          >
+            <el-option
+              v-for="item in userList"
+              :key="item.userId"
+              :label="item.userName"
+              :value="`${item.userId}/${item.userName}/${item.userEmail}`"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="负责人邮箱" prop="userEmail">
+          <el-input
+            placeholder="请输入负责人邮箱"
+            v-model="deptForm.userEmail"
+            disabled
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleClose">取消</el-button>
+          <el-button @click="handleSubmit" type="primary">确定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -75,10 +130,38 @@ export default {
         pageNum: 1,
         pageSize: 10,
       },
+      action: "create",
+      showModal: false,
+      deptForm: {},
+      userList: [],
+      rules: {
+        parentId: [
+          {
+            required: true,
+            message: "请选择上级部门",
+            trigger: "blur",
+          },
+        ],
+        deptName: [
+          {
+            required: true,
+            message: "请输入部门名称",
+            trigger: "blur",
+          },
+        ],
+        user: [
+          {
+            required: true,
+            message: "请选择负责人",
+            trigger: "blur",
+          },
+        ],
+      },
     }
   },
   mounted() {
     this.getDeptList()
+    this.getAllUserList()
   },
   methods: {
     async getDeptList() {
@@ -88,8 +171,53 @@ export default {
       })
       this.deptList = list
     },
+    async getAllUserList() {
+      this.userList = await api.getAllUserList()
+    },
+    handleUser(val) {
+      console.log("=>", val)
+      const [userId, userName, userEmail] = val.split("/")
+      Object.assign(this.deptForm, { userId, userName, userEmail })
+    },
     handleReset(form) {
       this.$refs[form].resetFields()
+    },
+    handleOpen() {
+      this.action = "create"
+      this.showModal = true
+    },
+    handleEdit(row) {
+      this.action = "edit"
+      this.showModal = true
+      this.$nextTick(() => {
+        Object.assign(this.deptForm, row, {
+          user: `${row.userId}/${row.userName}/${row.userEmail}`,
+        })
+      })
+    },
+    async handleDel(_id) {
+      this.action = "delete"
+      await api.deptOperate({ _id, action: this.action })
+      this.$message.success("删除成功")
+      this.getDeptList()
+    },
+    handleClose() {
+      this.showModal = false
+      this.handleReset("dialogForm")
+    },
+    handleSubmit() {
+      this.$refs.dialogForm.validate(async (valid) => {
+        if (valid) {
+          let params = { ...this.deptForm, action: this.action }
+          delete params.user
+          let res = await api.deptOperate(params)
+          if (res) {
+            this.$message.success("操作成功")
+            this.handleClose()
+            this.getDeptList()
+          }
+        }
+      })
     },
   },
 }
