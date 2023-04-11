0635db86c92cbd67523a0d3de74a07513f992a6a
fuyachen <294143413@qq.com>
2023-04-11 15:24:44 +0800

    404页面

--- src/router/index.js
+++ src/router/index.js
@@ -58,6 +58,14 @@ const routes = [
     },
     component: () => import("../views/Login.vue"),
   },
+  {
+    name: "404",
+    path: "/404",
+    meta: {
+      title: "页面不存在",
+    },
+    component: () => import("../views/404.vue"),
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/404.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="not-found">
+    <p>404 NOT FOUND</p>
+    <img src="../assets/images/404.png" alt="" />
+    <el-button @click="backHome" type="primary">返回首页</el-button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router"
+const router = useRouter()
+const backHome = () => {
+  router.push("/")
+}
+</script>
+
+<style lang="scss">
+.not-found {
+  display: flex;
+  flex-direction: column;
+  padding-top: 96px;
+  padding-bottom: 96px;
+  margin: auto;
+  max-width: 500px;
+  min-height: 100vh;
+  text-align: center;
+  justify-content: center;
+  p {
+    font-size: 2rem;
+    color: #aab9de;
+  }
+}
+</style>
